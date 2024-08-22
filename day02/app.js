b48f6a0250f8ba545305d35d17fb17e2cc0723a3
comstudyschool <comstudychool@gmail.com>
2024-08-22 11:21:55 +0900

    nodejs day02 멤버리스트 01

--- day02/app.js
+++ day02/app.js
@@ -25,6 +25,25 @@ app.get("/member", (req, res) => {
         res.end(html);
     });
 });
+app.get("/login", (req, res) => {
+    req.app.render("member/Login", {}, (err, html)=>{
+        res.end(html);
+    });
+});
+app.post("/login", (req, res) => {
+    res.redirect("/member");
+});
+
+app.get("/joinus", (req, res) => {
+    // 회원 가입 ejs 페이지 forward
+    req.app.render("member/Joinus", {}, (err, html)=>{
+        res.end(html);
+    });
+});
+app.post("/joinus", (req, res) => {
+    // 회원 가입 처리 후 목록으로 갱신
+    res.redirect("/member");
+});
 
 app.get("/gallery", (req, res) => {
     req.app.render("gallery/Gallery", {}, (err, html)=>{
