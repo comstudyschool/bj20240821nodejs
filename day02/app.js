d4a0b7d0957e5064543123c5eb780ffaac61a311
comstudyschool <comstudychool@gmail.com>
2024-08-22 10:33:36 +0900

    nodejs day02 프로젝트 구성 준비

--- day02/app.js
+++ day02/app.js
@@ -14,6 +14,30 @@ app.get("/home", (req, res) => {
     })
 });
 
+app.get("/profile", (req, res) => {
+    req.app.render("Profile", {}, (err, html)=>{
+        res.end(html);
+    })
+});
+
+app.get("/member", (req, res) => {
+    req.app.render("Member", {}, (err, html)=>{
+        res.end(html);
+    })
+});
+
+app.get("/gallery", (req, res) => {
+    req.app.render("Gallery", {}, (err, html)=>{
+        res.end(html);
+    })
+});
+
+app.get("/shop", (req, res) => {
+    req.app.render("Shop", {}, (err, html)=>{
+        res.end(html);
+    })
+});
+
 const server = http.createServer(app);
 server.listen(app.get('port'), ()=>{
     console.log(`Run on server >>> http://localhost:${app.get('port')}`);
