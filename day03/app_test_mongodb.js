05e7240c7465a5ec8dab2c96b97ddb1fb6a9243c
comstudyschool <comstudychool@gmail.com>
2024-08-23 18:14:35 +0900

    nodejs day03 mongodb 모듈 업데이트

--- day03/app_test_mongodb.js
+++ day03/app_test_mongodb.js
@@ -17,39 +17,40 @@ const app = express();
 const MongoClient = require('mongodb').MongoClient;
 
 app.set('port', 3000);
-
 app.set('view engine', 'ejs');
 app.set('views', __dirname + "/views");
 
 let db = null;
 
-const connectionDB = () => {
-    // 데이터베이스 연결 정보
-    var databaseUrl = 'mongodb://localhost:27017';
-    // 데이터베이스 연결
-    MongoClient.connect(databaseUrl, {useUnifiedTopology: true}, function (err, database) {
-        if (err) { throw err; }
+const connectionDB = async () => {
+    try {
+        const databaseUrl = 'mongodb://localhost:27017';
+        const client = await MongoClient.connect(databaseUrl);
         console.log('데이터베이스에 연결되었습니다. : ' + databaseUrl);
-        // database 변수에 할당할때
-        // 몽고디비3 이상에서는 db명을 지정해 주어야 한다.
-        db = database.db('vehicle');
-    });
-}
+        db = client.db('vehicle');
+    } catch (err) {
+        console.error('MongoDB 연결 실패:', err);
+    }
+};
 
-app.get('/car', (req, res) => {
+app.get('/car', async (req, res) => {
     if(db!==null) {
         const car = db.collection('car');
-        car.find({}).toArray(function(err, carList) {
-            //console.log(carList);
-            req.app.render('car_list', {carList}, (err, html) => {
+        const carList = await car.find({}).toArray();
+        req.app.render('car_list', {carList}, (err, html) => {
+            if (err) {
+                res.status(500).send('템플릿 렌더링 중 오류가 발생했습니다.');
+            } else {
                 res.end(html);
-            });
+            }
         });
+    } else {
+        res.status(500).send('데이터베이스 연결이 되어 있지 않습니다.');
     }
 });
 
 const server = http.createServer(app);
 server.listen(app.get('port'), () => {
-    console.log("서버 실행 중 ...");
+    console.log("서버 실행 중 ... http://localhost:" + app.get('port'));
     connectionDB();
 });
