e2cb9ba139ed49f8006c5d709259050427a52e8c
comstudyschool <comstudychool@gmail.com>
2024-08-27 09:38:28 +0900

    nodejs day04 mongodb ex

--- day04/src/server.js
+++ day04/src/server.js
@@ -65,8 +65,6 @@ app.post("/car", async (req, res)=> {
   }
 });
 
-
-
 const server = http.createServer(app);
 server.listen(app.get('port'), ()=>{
   console.log(`서버 실행 중 >>> http://localhost:${app.get('port')}`);
